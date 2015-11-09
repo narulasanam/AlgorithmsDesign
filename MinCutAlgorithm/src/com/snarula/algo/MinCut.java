dcf7394477daf29d8df50c350348b9257ebec8e7
Sanam Narula <s5narula@uwaterloo.ca>
2015-11-08 20:16:57 -0500

    working on MincutAlgo

--- MinCutAlgorithm/src/com/snarula/algo/MinCut.java
+++ MinCutAlgorithm/src/com/snarula/algo/MinCut.java
@@ -1,8 +1,38 @@
 package com.snarula.algo;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MinCut {
-	public static void main(String arg[]){
-		
+	public static void main(String arg[]) {
+		Map<Integer, List<Integer>> graph = getGraph("KargerMinCut");
+
+	}
+
+	private static LinkedHashMap<Integer, List<Integer>> getGraph(String path) {
+		// TODO Auto-generated method stub
+		Map<Integer, List<Integer>> graphMap = new LinkedHashMap<>();
+		try {
+			FileReader fileReader = new FileReader(path);
+			BufferedReader br = new BufferedReader(fileReader);
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] splitArray = line.split("(\\s)+");
+				List<Integer> adjList = new ArrayList<>();
+				for (int i = 1; i < splitArray.length; i++) 
+					adjList.add(Integer.parseInt(splitArray[i]));
+				graphMap.put(Integer.parseInt(splitArray[0]),adjList);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		};
+
+		return null;
 	}
 }

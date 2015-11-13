c4b6789dff55afe5bfaf315201800b718799cdc2
Sanam Narula <s5narula@uwaterloo.ca>
2015-11-12 23:44:04 -0500

    better solution

--- DP&Recursion/src/com/snarula/algo/LongestPalin.java
+++ DP&Recursion/src/com/snarula/algo/LongestPalin.java
@@ -3,7 +3,38 @@ package com.snarula.algo;
 public class LongestPalin {
 
 	public static void main(String[] args) {
-		System.out.println(getLongestPalin(null));
+		//System.out.println(getLongestPalin(null));
+		System.out.println(getLongestPalinOpt("sanas"));
+	}
+
+	public static String getLongestPalinOpt(String s) {
+		if (s == null) {
+			throw new IllegalArgumentException("String reference to null");
+		}
+
+		if (s.length() == 0 || s.length() == 1) {
+			return s;
+		}
+
+		String longestPalin = s.substring(0, 1);
+		int length = 1;
+		for (int i = 0; i < s.length(); i++) {
+			String str = getLongestPalinOpt(s, i, i);
+			if (str.length() > length) {
+				length = str.length();
+				longestPalin = str;
+			}
+		}
+		return longestPalin;
+	}
+
+	static String getLongestPalinOpt(String s, int i, int j) {
+		while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+			i--;
+			j++;
+		}
+		return s.substring(i+1, j);
+
 	}
 
 	public static String getLongestPalin(String s) {
@@ -24,7 +55,7 @@ public class LongestPalin {
 
 				int len = j - i;
 
-				String str = s.substring(i, j+1);
+				String str = s.substring(i, j + 1);
 				if (isPalindrome(str)) {
 					if (str.length() > longestLength) {
 						longestLength = str.length();

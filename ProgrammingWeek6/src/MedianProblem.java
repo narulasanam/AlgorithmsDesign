9272827c4198765dc5a36c760d626eec2a53377f
Sanam Narula <s5narula@uwaterloo.ca>
2015-11-28 21:35:27 -0500

    Setting up Week6 Project

---
+++ ProgrammingWeek6/src/MedianProblem.java
@@ -0,0 +1,12 @@
+
+import java.io.File;
+import java.util.Scanner;
+public class MedianProblem {
+	
+	File file = new File("Median.txt");
+	Scanner in = new Scanner(System.in);
+	
+	while(in.hasNextInt()){
+		
+	}
+}
